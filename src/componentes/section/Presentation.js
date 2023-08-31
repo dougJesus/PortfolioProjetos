8f7ec79d749f5a8b389ad807e9a332831dc7754e
dougJesus <douglas.jb@outlook.com.br>
2023-08-30 21:20:07 -0300

    Adicionando estilo ao menu apresentação

--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,15 +1,18 @@
+import styles from './Presentation.module.css'
+
 function Presentation (){
     return (
-        <div id="Presentation">
-            <h1>Aqui ficará a minha apresentação</h1>
-        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-        
-        
-        
+        <div className={styles.presentation} id="Presentation">
+            <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
+            <h1>Olá, eu sou o Douglas!</h1>
+            <p>
+                Sou um apaixonado por tecnologia e soluções inovadoras.<br/>
+                Como Product Manager, eu tenho o compromisso de resolver problemas<br/>
+                complexos e trazer resultados excepcionais para os negócios.<br/>
+                Meus projetos já geraram milhões de reais em receita anual<br/>
+                estou sempre em busca de novos desafios para superar.
+
+            </p>
         </div>
     )
 }
