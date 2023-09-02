016e5653b8c9cc3e913d6fa8c97c8e4318527f59
dougJesus <douglas.jb@outlook.com.br>
2023-09-02 11:35:59 -0300

    finalizando os Cards

--- src/componentes/section/Projects.js
+++ src/componentes/section/Projects.js
@@ -1,7 +1,29 @@
+
+import styles from './Projects.module.css'
+import Card from '../elements/Card'
+import lpdnc from '../../image/projects/lpdnc.svg'
+import portfolio from '../../image/projects/portfolio.svg'
+
 function Projects (){
     return (
-        <div id="Projects">
-            <h1>Aqui ficará os meus projetos</h1>
+        <div className={styles.projects} id="Projects">
+            <h1>Projetos</h1>
+            <Card
+            img={lpdnc}
+            title= "LP DNC"
+            tech= "HTML, CSS e JS"
+            description="Desenvolvimento de uma Landing Page para o lançamento da formação em tecnologia"
+            repo="https://www.google.com"
+            site="https://www.google.com"
+            />
+            <Card
+            img={portfolio}
+            title= "LP DNC"
+            tech= "HTML, CSS e JS"
+            description="Desenvolvimento de uma Landing Page para o lançamento da formação em tecnologia"
+            repo="https://www.google.com"
+            site="https://www.google.com"
+            />
         </div>
     )
 }
